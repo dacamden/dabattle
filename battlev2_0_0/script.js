c977feffbff4cb76752ba3542375cf4615162edf
Leo Childs <leo.childs@live.com>
2014-07-28 15:55:12 +0000

    Merge barcode generated stats and images with battle sequence

--- battlev2_0_0/script.js
+++ battlev2_0_0/script.js
@@ -2,19 +2,57 @@
 ////////////BATTLE SCRIPT////////////
 /////////////////////////////////////
 
+//Arrays
+
+var healtharr = [1000, 1200, 1300, 1400, 1500, 1600, 1700, 1800, 1900, 2000, 2210, 2200,
+    2300, 2400, 2500, 2600, 2700, 2800, 2900, 3000
+];
+var defarr = [1, 2, 3, 4, 5];
+var attackarr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
+    19, 20
+];
+var imagearr = ["http://cdn.bulbagarden.net/upload/b/b8/059Arcanine.png",
+    "http://cdn.bulbagarden.net/upload/c/cd/024Arbok.png",
+    "http://cdn.bulbagarden.net/upload/c/cc/147Dratini.png",
+    "http://cdn.bulbagarden.net/upload/archive/f/f8/20100417191927%21187Hoppip.png",
+    "http://cdn.bulbagarden.net/upload/archive/5/5d/20130914032735%21010Caterpie.png",
+    "http://blog.dacamden.co.uk/images/leo.JPG",
+    "http://studio101.biz/wp-content/plugins/files/civicrm/persist/contribute/images/Monsters%20vs%20Aliens%20035.jpg",
+    "https://lh3.googleusercontent.com/-lWO0Yu4OHJc/TXpEikJmhII/AAAAAAAAAe0/BWobw90XdlM/s1600/March-of-the-Monsters-Day-11.jpg","https://stevenchasestudios.files.wordpress.com/2012/10/yeti-2.jpg"
+];
+
+// BARCODE GENERATOR
+var barcode1 = Math.floor((function getRandomArbitrary() {
+    return Math.random() * (9999999999999 - 1111111111111) + 1;
+})());
+var barcode2 = Math.floor((function getRandomArbitrary() {
+    return Math.random() * (9999999999999 - 1111111111111) + 1;
+})());
+var formmaker = function(code) {
+    var barcodex = code.toString();
+    console.log(barcode1);
+    var n = barcodex.split('').map(Number);
+    console.log(n);
+    return Math.round(((n[2] + 3 + n[6] + 9 + n[7] + 5) / (n[0] + 50)) * 81);
+};
+var form1 = formmaker(barcode1),
+    form2 = formmaker(barcode2);
+
 //Declare Player Stat Variables
 
-var pHitPoints = 500,
-    pBaseDmg = 5,
-    pDefPoints = 1,
-    pMit = Math.log(pDefPoints) / Math.log(1.1);
+var pHitPoints = healtharr[form1 % 20],
+    pBaseDmg = attackarr[form1 % 20],
+    pDefPoints = defarr[form1 % 5],
+    pMit = Math.log(pDefPoints) / Math.log(1.1),
+    pImg = imagearr[form1 % 9];
 
 //Declare Monster Stat Variables
 
-var cHitPoints = 500,
-    cBaseDmg = 5,
-    cDefPoints = 1,
-    cMit = Math.log(cDefPoints) / Math.log(1.1);
+var cHitPoints = healtharr[form2 % 20],
+    cBaseDmg = attackarr[form2 % 20],
+    cDefPoints = defarr[form2 % 5],
+    cMit = Math.log(cDefPoints) / Math.log(1.1),
+    cImg = imagearr[form2 % 9];
 
 //Declare Turn variable
 
@@ -32,11 +70,19 @@ $(document).ready(function(){
   $('#cDefPoints').html(Math.round(cDefPoints));
   $('#cMit').html(Math.round(cMit));
   $('#Turn').html(turn);
+  $('#pImg').html("<img src=" + pImg + " height=" + 200 +
+        " width=" + 200 + ">");
+  $('#cImg').html("<img src=" + cImg + " height=" + 200 +
+        " width=" + 200 + ">");
 });
 
 //Declare Button Functions
 
 pAttack1 = function foo (power) {
+  if (pHitPoints < 1) {
+    pHitPoints = 'Dead';
+    $('#pHitPoints').html(pHitPoints);} 
+  else {
   if ((turn%2) === 1){
     var dmg = pBaseDmg * power * (1 - (cMit/100));
     cHitPoints =  cHitPoints - dmg;
@@ -44,6 +90,10 @@ pAttack1 = function foo (power) {
     $('#PlayerLog').html("<p style = 'color: red;'>Player attacked Computer!</p>");
     turn = turn + 1;
     $('#Turn').html(turn);
+    if (cHitPoints < 1) {
+    cHitPoints = 'Dead';
+    $('#cHitPoints').html(cHitPoints);
+  } else {
     $('#AiLog').html("<p style = 'color: blue;'>Computer is choosing a Move</p>");
     var delay=2500; //2.5 seconds
     setTimeout(function(){
@@ -71,17 +121,21 @@ pAttack1 = function foo (power) {
       $('#AiLog').html("<p style = 'color: green;'>Computer increased defense</p>");
       turn = turn + 1;
       $('#Turn').html(turn); 
-    };
-  } else {
-    $('#AiLog').html("<p>Waiting...</p>");
-  }
-    },delay); 
+    }
+  } 
+    },delay);} 
   } else {
     $('#PlayerLog').html("<p>It is not your turn</p>");
-  };
+  }
+  }
+  
 };
 
 pAttack2 = function foo (power) {
+  if (pHitPoints < 1) {
+    pHitPoints = 'Dead';
+    $('#pHitPoints').html(pHitPoints);}
+  else {
   if ((turn%2) === 1){
     var dmg = pBaseDmg * power * (1 - (cMit/100));
     cHitPoints =  cHitPoints - dmg;
@@ -89,6 +143,10 @@ pAttack2 = function foo (power) {
     $('#PlayerLog').html("<p style = 'color: red;'>Player attacked Computer!</p>");
     turn = turn + 1;
     $('#Turn').html(turn);
+    if (cHitPoints < 1) {
+    cHitPoints = 'Dead';
+    $('#cHitPoints').html(cHitPoints);
+  } else {
     $('#AiLog').html("<p style = 'color: blue;'>Computer is choosing a Move</p>");
     var delay=2500; //2.5 seconds
     setTimeout(function(){
@@ -116,17 +174,20 @@ pAttack2 = function foo (power) {
       $('#AiLog').html("<p style = 'color: green;'>Computer increased defense</p>");
       turn = turn + 1;
       $('#Turn').html(turn); 
-    };
-  } else {
-    $('#AiLog').html("<p>Waiting...</p>");
-  }
-    },delay); 
+    }
+      } 
+    },delay); }
   } else {
     $('#PlayerLog').html("<p>It is not your turn</p>");
-  };
+  }
+  }
 };
 
 pDef = function foo (power){
+  if (pHitPoints < 1) {
+    pHitPoints = 'Dead';
+    $('#pHitPoints').html(pHitPoints);} 
+  else {
   if ((turn%2) ===1) {
     pDefPoints = pDefPoints + power;
     pMit = Math.log(pDefPoints) / Math.log(1.1);
@@ -169,7 +230,8 @@ pDef = function foo (power){
     },delay); 
   } else {
     $('#PlayerLog').html("<p>It is not your turn</p>");
-  };
+  }
+  }
 };
 
 //Computer Buttons for testing
